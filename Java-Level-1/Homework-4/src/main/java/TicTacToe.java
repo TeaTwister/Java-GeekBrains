c8f9dc1996ebd6c90bbffd9ca89ee43f07552336
VS <72758495+TeaTwister@users.noreply.github.com>
2020-10-23 21:12:30 +0300

    Homework 4 initial, not ready

--- Java-Level-1/Homework-4/src/main/java/TicTacToe.java
+++ Java-Level-1/Homework-4/src/main/java/TicTacToe.java
@@ -1,5 +1,14 @@
 public class TicTacToe {
-    public static void main(String[] args) {
+    public static int fieldSide;
+    static char[][] field;
 
+    public static void main(String[] args) {
+        fieldSide = 3;
+        field = emptyField(fieldSide);
+
+    }
+
+    private static char[][] emptyField(int side) {
+        field = new char[side][side];
     }
 }
