ae3cb06915c831a42700ae9c7c71b802194626fc
VS <72758495+TeaTwister@users.noreply.github.com>
2021-01-18 12:55:46 +0300

    Homework 5

---
+++ Java-Level-3/src/main/java/homework5/Car.java
@@ -0,0 +1,60 @@
+package homework5;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Car implements Runnable {
+    private static final ReentrantLock photoFinish = new ReentrantLock();
+    private static int CARS_COUNT;
+    private static CountDownLatch startLine;
+    private static CountDownLatch finishLine;
+
+    static {
+        CARS_COUNT = 0;
+    }
+
+    private final Race race;
+    private final int speed;
+    private final String name;
+
+    public Car(Race race, int speed) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+    }
+
+    public static void setStartLine(CountDownLatch startLine) {
+        Car.startLine = startLine;
+    }
+
+    public static void setFinishLine(CountDownLatch finishLine) {
+        Car.finishLine = finishLine;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int) (Math.random() * 800));
+            System.out.println(this.name + " готов");
+            startLine.countDown();
+            startLine.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+        }
+        if (photoFinish.tryLock()) System.out.println("Победитель - " + name + "!");
+        finishLine.countDown();
+    }
+}
