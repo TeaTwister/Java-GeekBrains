1f17c1762a848f401ee416d75c3d8c669de5c097
VS <72758495+TeaTwister@users.noreply.github.com>
2021-01-18 12:55:46 +0300

    Homework 5

--- Java-Level-3/src/main/java/homework5/MainClass.java
+++ Java-Level-3/src/main/java/homework5/MainClass.java
@@ -1,19 +1,35 @@
 package homework5;
 
+import java.util.concurrent.CountDownLatch;
+
 public class MainClass {
     public static final int CARS_COUNT = 4;
 
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        CountDownLatch startLine = new CountDownLatch(CARS_COUNT);
+        CountDownLatch finishLine = new CountDownLatch(CARS_COUNT);
+        Car.setStartLine(startLine);
+        Car.setFinishLine(finishLine);
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
         }
-        for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
+        for (Car car : cars) {
+            new Thread(car).start();
+        }
+        try {
+            startLine.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        try {
+            finishLine.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
